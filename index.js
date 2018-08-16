70d1184df2bf2654162966a956bd234c817b9709
jake.c.rummel@gmail.com <jake.c.rummel@gmail.com>
2018-08-16 23:36:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+ function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  [...kittens, "Broom"]
+  return kittens
+}
+
+function removeLastKitten() {
+kittens.slice(1)
+return kittens
+}
+
+function removeFirstKitten() {
+kittens.slice(1,2)
+return kittens 
+}
